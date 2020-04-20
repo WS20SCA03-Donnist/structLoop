4d8dfb9305d041f40d7883222b5290b43c41825c
Donavin Wats <donnist@gmail.com>
2020-04-20 12:19:14 -0400

    Initial Commit

---
+++ structLoop/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  structLoop
+//
+//  Created by Donavin Watson on 4/20/20.
+//  Copyright © 2020 Don Watsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
